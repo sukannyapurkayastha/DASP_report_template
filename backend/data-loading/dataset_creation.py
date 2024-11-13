e59904721bd36f8bd922988e5fa7a2ad22f7183b
JanWerth <jan9901@hotmail.de>
2024-11-13 16:11:02 +0100

    (Wrapper) Class to load reviews, papers, and venues from the OpenReview api v2 (only works for ICLR 2024 yet) Create test set from the reviews of two random papers

---
+++ backend/data-loading/dataset_creation.py
@@ -0,0 +1,17 @@
+import argparse
+import pandas as pd
+
+from openreview_loader import OpenReviewLoader
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("username", help="Openreview username")
+    parser.add_argument("password", help="Openreview password")
+
+    args = parser.parse_args()
+
+    client = OpenReviewLoader(username=args.username, password=args.password)
+    test_set = client.create_testset(ids=["zzv4Bf50RW", "KS8mIvetg2"])
+
+    test_set = pd.DataFrame(test_set, columns=["sentences"])
+    test_set.to_csv("data/test_set.csv", index=False, encoding="utf-8")
