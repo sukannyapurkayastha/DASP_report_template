34e0017f8723ca59aca7c449182a34598e1cd39d
carmenappelt <82046123+carmenappelt@users.noreply.github.com>
2024-12-03 20:14:56 +0100

    back to old

--- frontend/main_page.py
+++ frontend/main_page.py
@@ -192,14 +192,15 @@ def main_page(custom_css):
     
     st.title("Paper Review Summary")
 
-    attitude_roots = get_classification_with_url()
-    
-    with open(os.path.join(base_path, 'dummy_data', 'dummy_overview.pkl'), 'rb') as file:
+   # attitude_roots = get_classification_with_url()
+    with open(os.path.join(base_path, 'frontend/dummy_data', 'dummy_attitude_roots.pkl'), 'rb') as file:
+        attitude_roots = pickle.load(file)
+    with open(os.path.join(base_path, 'frontend/dummy_data', 'dummy_overview.pkl'), 'rb') as file:
         overview = pickle.load(file)
-    with open(os.path.join(base_path, 'dummy_data', 'dummy_requests.pkl'), 'rb') as file:
+    with open(os.path.join(base_path, 'frontend/dummy_data', 'dummy_requests.pkl'), 'rb') as file:
         request_information = pickle.load(file)
 
-    summary = pd.read_csv(os.path.join(base_path,"dummy_data", "dummy_summary.csv"), sep=";", encoding="utf-8")
+    summary = pd.read_csv(os.path.join(base_path,"frontend/dummy_data", "dummy_summary.csv"), sep=";", encoding="utf-8")
     
     use_default_container(modules.overview.show_overview, overview)
     attitude_root_container = lambda: modules.attitude_roots.show_attitude_roots_data(attitude_roots)
