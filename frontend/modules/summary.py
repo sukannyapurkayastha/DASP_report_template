88aaa65b1bc300dcac9ae369b4ac3be4460fe7fe
JanWerth <jan9901@hotmail.de>
2025-01-10 16:19:14 +0100

    More streamlit tests

--- frontend/modules/summary.py
+++ frontend/modules/summary.py
@@ -1,14 +1,12 @@
 import streamlit as st
-import pandas as pd
 
 
 def show_summary_data(summary_data):
-
-
     with st.container():
         st.markdown('<div class="invisbible-line-minor">  </div>', unsafe_allow_html=True)
         if summary_data.empty:
-            st.markdown("<h4 style='font-size:16px; margin: 0px; padding: 0px;'>No Summary was found.</h4>", unsafe_allow_html=True)
+            st.markdown("<h4 style='font-size:16px; margin: 0px; padding: 0px;'>No Summary was found.</h4>",
+                        unsafe_allow_html=True)
         else:
             for index, row in summary_data.iterrows():
-                st.write(row[0]) # write summary
+                st.write(row[0])  # write summary
