d9403243457b0d8cf1c8d6e3948345e79a036774
Zhijingshui Yang <zhijingshui.yang@gmail.com>
2025-02-17 23:16:44 +0000

    desc change model path

--- attitude_classifier/test_desc_generatoe.py
+++ attitude_classifier/test_desc_generatoe.py
@@ -2,8 +2,9 @@ from transformers import T5Tokenizer, T5ForConditionalGeneration
 import torch
 
 # Load the pretrained model and tokenizer
-local_path = "models/description_generator/"
-huggingface_model_path = "DASP-ROG/ThemeModel"
+
+local_path = "/opt/models/attitude/description_generator"
+huggingface_model_path = "DASP-ROG/DescriptionModel"
 tokenizer = T5Tokenizer.from_pretrained(huggingface_model_path, cache_dir=local_path)
 model = T5ForConditionalGeneration.from_pretrained(huggingface_model_path, cache_dir=local_path, torch_dtype=torch.float16)
 
